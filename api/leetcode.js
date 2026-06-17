1128755ecc5c589f57d0ddd540e89a781a660758
Vaishnavi Waghmare <your_github_email@example.com>
2026-06-17 22:20:46 +0530

    Add Redis caching for LeetCode profiles

--- api/leetcode.js
+++ api/leetcode.js
@@ -2,19 +2,24 @@ const redis = require("../lib/redis");
 
 export default async function handler(req, res) {
 
-  await redis.set("test", "hello");
-
-  const value = await redis.get("test");
-
-  console.log(value);
-
-
   // Only allow POST requests
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
 
   try {
+    const username = req.body?.variables?.username;
+    const cacheKey = `leetcode:${username}`;
+
+    const cachedData = await redis.get(cacheKey);
+
+      if (cachedData) {
+          console.log(`CACHE HIT: ${username}`);
+
+          return res.status(200).json(cachedData);
+      }
+
+      console.log(`CACHE MISS: ${username}`);
     // This runs on Vercel's server, bypassing browser CORS entirely!
     const response = await fetch('https://leetcode.com/graphql', {
       method: 'POST',
@@ -26,6 +31,18 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
+
+      if (
+        data.data &&
+        data.data.matchedUser
+    ) {
+        await redis.set(cacheKey, data, {
+            ex: 3600,
+        });
+
+        console.log(`Stored ${username} in cache`);
+    }
+
     return res.status(200).json(data);
     
   } catch (error) {
